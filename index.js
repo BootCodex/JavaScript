1904e3af24a0d8624b30bcd45d618c21065bacf7
McMillan <117737074+BootCodex@users.noreply.github.com>
2023-01-16 15:48:41 +0200

    WORK

---
+++ index.js
@@ -0,0 +1,45 @@
+alert("it`s` working")
+// const step1 = Math.random();
+
+// const step2 = step1 * 10
+
+// const step3 = Math.floor(step2);
+
+// const step4 = step3 + 1;
+
+// console.log(step4)
+
+// comparisons 
+
+//  > 
+
+// const age = 16
+
+// if (age >= 10) {
+//     console.log ("old enough")
+// } else {
+//     console.log("too young")
+// }
+
+// console.log(age)
+
+
+let rating = 1;
+
+if (rating === 3) {
+    console.log("You Are A Superstar!!!");
+}
+else if (rating === 2) {
+    console.log("MEETS EXPECTATIONS");
+}
+else if (rating === 1) {
+    console.log("NEEDS IMPROVEMENT")
+}
+
+let num = 38;
+if (num % 2 !== 0) {
+    console.log("ODD NUMBER");
+} else {
+    console.log("EVEN NUMBER");
+}
+
